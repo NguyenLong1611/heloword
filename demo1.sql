b8b0a6b9f86daa7621cab7891cebe2332013d17b
NguyenLong1611 <134915016+NguyenLong1611@users.noreply.github.com>
2023-05-29 14:11:03 +0700

    Add files via upload

---
+++ demo1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE yen_khanh(
+ten NVARCHAR2(50),
+so_dt char(20),
+dia_chi NVARCHAR2(50)
+);
+INSERT INTO yen_khanh VALUES ('Toàn','039876345','UDIC');
+INSERT INTO yen_khanh VALUES ('L?c','039876345','C?u Di?n');
+INSERT INTO yen_khanh VALUES ('Long','039876345','Ng? Nh?c');
+SELECT * FROM yen_khanh;
